d7cd6bc7725eb9f82471253fc3bbca6a22542fd6
mattkrebs2000 <mattkrebsemail@gmail.com>
2024-12-23 13:25:55 -0500

    hi

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5002;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.get('/api/hello', (req, res) => {
+  res.send({ message: "Hello from server!!!!!!!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
